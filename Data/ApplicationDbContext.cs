1940ca62680669d559c0a9ef578053e8017cbd7d
rezhang1128 <ryenzhang1128@gmail.com>
2025-07-12 23:13:06 +1000

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleProject.Models;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Patient> Patients { get; set; }
+}
---
+++ Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleProject.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DOB { get; set; }
+
+        public string Address { get; set; }
+    }
+}
